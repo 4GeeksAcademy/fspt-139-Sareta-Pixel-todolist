ebefb8110a6c67dff0017ac08a516a315d3fb770
SaraMarti <saramartialcocel@gmail.com>
2026-07-14 12:55:10 +0000

    toDoList Done

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [inputTarea, setInputTarea] = useState("")
+	const [tareas, setTareas] = useState([])
+	const eliminarTarea = (index) => {
+		setTareas(tareas.filter((tarea, i) => i !== index))
+	
+	}
+
+
+return (
+
+	<div className="toDoList">
+		<p className="titulo">Lista de Tareas para hoy<span className="icono"><i class="fa-solid fa-list-check"></i></span></p>
+		<input className="input"
+			value={inputTarea}
+			type="text"
+			placeholder="Tarea pendiente"
+			onChange={event => setInputTarea(event.target.value)}
+			onKeyUp={event => {
+				if (event.key === "Enter") {
+					setTareas([...tareas, inputTarea])
+					setInputTarea("")
+				}
+			}}
+		/>
+		<ul>
+			{
+				tareas.map((tarea, index) => {
+					return (
+						<li key={index} >
+							<span className="texto">{tarea} </span>
+							<span className="cruz" onClick={(event)=> eliminarTarea(index)}>
+								<i class="fa-solid fa-xmark"></i>
+							</span>
+						</li>
+					)
+				})
+			}
+		</ul>
+		<div>
+			<p className="tareas-pendientes">
+				{tareas.length === 0 ? ("No tienes tareas pendientes") : `Tienes ${tareas.length} pendientes`}
+						
+				</p>
+
+
 		</div>
-	);
+	</div>
+);
 };
 
 export default Home;
